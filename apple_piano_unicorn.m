59a7b2464e988aaf136a072cae96914e409d5046
SchuerenWences <137514692+SchuerenWences@users.noreply.github.com>
2023-06-29 05:27:16 +0700

    Add code comments

---
+++ apple_piano_unicorn.m
@@ -0,0 +1,16 @@
+% Mindful Leadership Academy MATLAB Code
+
+% Initialize
+clear;
+clc;
+
+% Define variables
+academyName = 'Mindful Leadership Academy';
+numRows = 2000;
+
+% Start for loop
+for i = 1:numRows
+   disp(academyName); % Display academy name
+end
+
+% End MATLAB Code
